46fcf114e5293903e8ee16ae920794346d51face
Jellooking <jassimkhalaf14@gmail.com>
2024-05-26 12:54:27 +0300

    Implemented Google Oauth

--- GamerWish/routes/games.js
+++ GamerWish/routes/games.js
@@ -2,14 +2,17 @@ const express = require('express')
 const router = express.Router()
 const gameCtrl = require('../controllers/games')
 const reviewCtrl = require('../controllers/reviews')
+const ensureLoggedIn = require('../config/ensureLoggedIn')
+
 // GET /games
 router.get('/', gameCtrl.index)
 // GET /games/new
-router.get('/new', gameCtrl.new)
+router.get('/new', ensureLoggedIn, gameCtrl.new)
 // GET /games/:id (show functionality) MUST be below new route
 router.get('/:id', gameCtrl.show)
 // POST /games
 router.post('/', gameCtrl.create)
+
 router.post('/games/:gameId/reviews', reviewCtrl.createReview)
 
 module.exports = router
--- GamerWish/routes/index.js
+++ GamerWish/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express')
-const passport = require('passport');
+const passport = require('passport')
 
 var router = express.Router()
 const check = 'https://api.stripe.com'
@@ -9,32 +9,35 @@ router.get('/', async (req, res, next) => {
   res.render('index', { title: 'Gamer Wish' })
 })
 
+module.exports = router
+
 // Google OAuth login route
-router.get('/auth/google', passport.authenticate(
-  // Which passport strategy is being used?
-  'google',
-  {
-    // Requesting the user's profile and email
-    scope: ['profile', 'email'],
-    // Optionally force pick account every time
-    // prompt: "select_account"
-  }
-));
+router.get(
+  '/auth/google',
+  passport.authenticate(
+    // Which passport strategy is being used?
+    'google',
+    {
+      // Requesting the user's profile and email
+      scope: ['profile', 'email']
+      // Optionally force pick account every time
+      // prompt: "select_account"
+    }
+  )
+)
 
 // Google OAuth callback route
-router.get('/oauth2callback', passport.authenticate(
-  'google',
-  {
-    successRedirect: '/index',
-    failureRedirect: '/index'
-  }
-));
+router.get(
+  '/oauth2callback',
+  passport.authenticate('google', {
+    successRedirect: '/games',
+    failureRedirect: '/games'
+  })
+)
 
 // OAuth logout route
-router.get('/logout', function(req, res){
-  req.logout(function() {
-    res.redirect('/index');
-  });
-});
-
-module.exports = router
+router.get('/logout', function (req, res) {
+  req.logout(function () {
+    res.redirect('/games')
+  })
+})
