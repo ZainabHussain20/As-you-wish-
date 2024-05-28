4efa0c74ef96ea2278212391ce6eb8f83d84e93c
m00hammed <m00hammed776@gmail.com>
2024-05-28 10:03:06 +0300

    Iprepared the server for new page

--- GamerWish/routes/new.js
+++ GamerWish/routes/new.js
@@ -3,3 +3,5 @@ const router = express.Router()
 const newCtrl = require('../controllers/new')
 
 router.get('/new', newCtrl)
+
+module.new = router
--- GamerWish/server.js
+++ GamerWish/server.js
@@ -15,6 +15,7 @@ var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 const gameRouter = require('./routes/games')
 const wishListRouter = require('./routes/wishLists')
+const newRouter = require('./routes/new')
 var app = express()
 
 // view engine setup
@@ -56,6 +57,7 @@ app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/games', gameRouter)
 app.use('/wishList', wishListRouter)
+app.use('/new', newRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
